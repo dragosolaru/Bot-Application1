0aa4fd5db06cb45d9d524d229547df6e98944db7
Dragos Andrei Olaru <DragosAndreiOlaru@gmail.com>
2017-04-02 20:22:31 +0300

    Add project files.

---
+++ Bot Application1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BotStock.WebApiApplication" Language="C#" %>
---
+++ Bot Application1/Services/StockService.cs
@@ -0,0 +1,40 @@
+﻿using BotStock.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BotStock.Services
+{
+    public class StockService
+    {
+        public static async Task<StockItem> GetStockPrice(string symbol)
+        {
+            string uri = $"http://dev.markitondemand.com/Api/v2/Quote/json?symbol={symbol}";
+            var stockItem = new StockItem();
+            using (var client = new WebClient())
+            {
+                var rawData = await client.DownloadStringTaskAsync(new Uri(uri));
+                stockItem = JsonConvert.DeserializeObject<StockItem>(rawData);
+                stockItem.Status = stockItem.Status ?? "FAIL";
+                return stockItem;
+            }
+                 return null;
+
+               /* if (stockItem.Status == "FAIL")
+                {
+                    return stockItem;
+                }
+                else
+                {
+                    return null;
+                }*/
+
+            
+
+        }
+    }
+}
